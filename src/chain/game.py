eaf5b49ae7096c02d0703c2afb8f0f9988aa6959
CJ Carey <perimosocordiae@gmail.com>
2010-02-25 05:53:26 +0000

    random column generation

--- src/chain/game.py
+++ src/chain/game.py
@@ -1,6 +1,6 @@
 MODEL_PATH = "../../models"
 
-from random import randint
+from random import randint, choice
 #from itertools import product as iproduct
 import direct.directbase.DirectStart
 from eventHandler import GameEventHandler
@@ -38,8 +38,9 @@ class Game(object):
         self.drones[str(hash(d))] = d 
 
     def load_env(self):
+        eggs = map(lambda s:"%s/%s_floor.egg"%(MODEL_PATH,s),['yellow','blue','green','red'])
         num_tiles = self.map_size/self.tile_size
-        environ = loader.loadModel("%s/yellow_floor.egg"%MODEL_PATH)
+        environ = loader.loadModel(eggs[0])
         environ.reparentTo(render)
         environ.setScale(self.tile_size, self.tile_size, self.tile_size)
         environ.setPos(0, 0, 0)
@@ -49,7 +50,7 @@ class Game(object):
         #for i,j in iproduct(range(num_tiles),repeat=2):
         for i in range(num_tiles):
           for j in range(num_tiles):
-            egg = get_tile_egg(i,j,center)
+            egg = eggs[egg_index(i,j,center)]
             make_tile(environ,egg,(-2*(1+i-center),-2*(1+j-center), 2*wall_height), (0, 0, 180)) #ceiling
             if i == center and j == center: continue #bottom center is already done
             make_tile(environ,egg,(2*(i-center), 2*(j-center), 0),(0, 0, 0)) #floor
@@ -57,34 +58,33 @@ class Game(object):
         #for i,j in iproduct(range(num_tiles),range(wall_height)):
         for i in range(num_tiles):
           for j in range(wall_height):
-            egg = get_tile_egg(i,j,center)
+            egg = eggs[egg_index(i,j,center)]
             make_tile(environ,egg,(-1-2*center,    2*(i-center),  2*(wall_height-j)-1),(0, 0, 90))  #wall 1
             make_tile(environ,egg,(-2*(1+i-center), -1-2*center,  2*(wall_height-j)-1),(0,-90,0))   #wall 2
             make_tile(environ,egg,( 2*center-1,     2*(i-center), 2*j+1),            (0, 0, -90)) #wall 3
             make_tile(environ,egg,(-2*(1+i-center), 2*center-1,   2*j+1),            (0, 90,0))   #wall 4
-
-		# random column 
-		#TODO: abstract this out
-        blue_egg = "%s/blue_floor.egg"%MODEL_PATH
-        for z in range(center / 2):
-            make_tile(environ,blue_egg,(center,   center,   2*z+1),(0,  90,0))
-            make_tile(environ,blue_egg,(center+1, center+1, 2*z+1),(90, 90,0))
-            make_tile(environ,blue_egg,(center,   center+2, 2*z+1),(180,90,0))
-            make_tile(environ,blue_egg,(center-1, center+1, 2*z+1),(270,90,0))
+            
+        # make some random bunkers
+        for _ in range(4):
+            make_column(environ, choice(eggs), randint(-num_tiles,num_tiles), randint(-num_tiles,num_tiles), randint(2,wall_height))
+        
+def make_column(parent,egg,x,y,h):
+    for z in range(h):
+        make_tile(parent,egg,(x,   y,   2*z+1),(0,  90,0))
+        make_tile(parent,egg,(x,   y+2, 2*z+1),(180,90,0))
+        make_tile(parent,egg,(x+1, y+1, 2*z+1),(90, 90,0))
+        make_tile(parent,egg,(x-1, y+1, 2*z+1),(270,90,0))
     
 # static functions, not in the game class
 def make_tile(parent,fname,pos,hpr):
-	tile = loader.loadModel(fname)
-	tile.reparentTo(parent)
-	tile.setScale(1.0, 1.0, 1.0)
-	tile.setPos(*pos)
-	tile.setHpr(*hpr)
+    tile = loader.loadModel(fname)
+    tile.reparentTo(parent)
+    tile.setScale(1.0, 1.0, 1.0)
+    tile.setPos(*pos)
+    tile.setHpr(*hpr)
 
-def get_tile_egg(i,j,center):
-	if i < center and j < center:
-		return "%s/blue_floor.egg"%MODEL_PATH
-	if i < center:
-		return "%s/green_floor.egg"%MODEL_PATH
-	if j < center:
-		return "%s/red_floor.egg"%MODEL_PATH
-	return "%s/yellow_floor.egg"%MODEL_PATH
+def egg_index(i,j,center):
+    if i < center and j < center: return 1
+    if i < center: return 2
+    if j < center: return 3
+    return 0
