2f1e9645476c44151045c4212aa94aef88a45a6a
CJ Carey <perimosocordiae@gmail.com>
2010-03-21 04:26:29 +0000

    oops

--- src/chain/shell.py
+++ src/chain/shell.py
@@ -190,7 +190,7 @@ class Shell(object):
     def main(self,port_num,ip,last=False):
         print "starting up"
         g = Game(ip,port_num,360,60.0,12.0,120)
-        self.client.send("player %s"%uname()[1])   
+        g.client.send("player %s"%uname()[1])   
         if last:
             g.client.send("start")
         
